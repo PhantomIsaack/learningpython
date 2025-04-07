c46eb6134bd32eaeae2adabb270dc9178c64bc09
PhantomIsaack <puedesermanolo@gmail.com>
2025-04-07 02:56:58 -0600

    primera practica

---
+++ practica_variables_y_tipos_leccion1.py
@@ -0,0 +1,9 @@
+nombre = "Alejandro"
+edad = 22
+estatura = 1.75
+estudiante = True
+
+mensaje_estudiante = "soy" if estudiante else "no soy"
+
+print(f"Hola, soy {nombre}, tengo {edad} años, mido {estatura} metros y actualmente {mensaje_estudiante} estudiante.")
+
