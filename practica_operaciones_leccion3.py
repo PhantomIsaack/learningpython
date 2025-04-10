09de66ba342512574e8cda210b9034c10b22db9f
PhantomIsaack <puedesermanolo@gmail.com>
2025-04-10 00:11:54 -0600

    operadores matematicos

---
+++ practica_operaciones_leccion3.py
@@ -0,0 +1,13 @@
+a = int(input("ingresa un numero entero"))
+b = int(input("ingres otro numero entero"))
+
+print("la suma es:", a+b)
+print("la resta es:", a-b)
+print("la multiplicacion es:", a*b)
+print("la division es:", a/b)
+print("la division entera es:", a//b)
+print("la potencia de los dos numeros es:", a**b)
+
+edad = int(input("ingresa tu edad"))
+es_mayor = edad >= 18
+print("es mayor de edad", es_mayor)
