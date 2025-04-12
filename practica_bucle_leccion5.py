37a5c171fc7af24e1a145cd47ac5c4f327c72f56
PhantomIsaack <puedesermanolo@gmail.com>
2025-04-11 23:59:03 -0600

    primer intento

---
+++ practica_bucle_leccion5.py
@@ -0,0 +1,9 @@
+a = int(input("Ingrese un numero entero positivo: "))
+for i in range (1, a):
+	print(i)
+	 if i%3=1:
+		print("Fizz")
+	elif i%5=1:
+		print("Buzz")
+	elif i%3=1 and i%5=1:
+		print("FizzBuzz")
