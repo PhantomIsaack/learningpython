c0b2f10399e78ca630305dd720a3173a20a4e2b1
PhantomIsaack <puedesermanolo@gmail.com>
2025-04-12 23:39:14 -0600

    Uso de listas, diccionario y tuplas con vairbales

---
+++ practica_listas_tuplas_diccionarios_leccion6.py
@@ -0,0 +1,11 @@
+amigo1 = input("ingresa el nombre de un amigo: ")
+amigo2 = input("ingresa el nombre de un segundo amigo: ")
+amigo3 = input("ingresa el nombre de un tercer amigo: ")
+
+lista_de_amigos = [amigo1, amigo2, amigo3]
+tupla_de_amigos = (amigo1, amigo2, amigo3)
+diccionario_de_amigos = {'amigo1': amigo1, 'amigo2': amigo2, 'amigo3': amigo3}
+
+print(lista_de_amigos)
+print(tupla_de_amigos)
+print(diccionario_de_amigos)
